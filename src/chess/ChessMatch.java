15698230bdd0059a3746dd925d8806cd7b23b4a5
Gabriel Klein <gabriel.klein@philips.com>
2019-03-11 06:51:39 -0300

    Add files via upload

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Posição inválida");
 		}
+		if (!board.piece(position).isTherePossibleMove()) {
+			throw new ChessException("Nao existem movimentos possiveis para a peca");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
